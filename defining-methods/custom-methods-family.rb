63fa43193e2d0c0342a4f6597962f9befe00076c
Philip White <phwhite89@gmail.com>
2022-07-26 16:08:53 +0100

    custom method practice with strings.

---
+++ defining-methods/custom-methods-family.rb
@@ -0,0 +1,6 @@
+def myFamily(person1, person2, person3, person4, person5)
+    return "My family members are: #{person1}, #{person2}, #{person3}, #{person4} and #{person5}."
+end
+
+familyMembers = myFamily("Kat", "Dad", "Louisa", "William", "me")
+puts familyMembers

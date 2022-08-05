b57919a2c80ab5903be2122119b66d7105542396
Philip White <phwhite89@gmail.com>
2022-08-05 11:22:28 +0100

    added blocks working explanation and reference.

---
+++ arrays-blocks/blocks-reference.rb
@@ -0,0 +1,16 @@
+numbers = [43, 2, 6, 33]
+
+p numbers
+
+add_one = numbers.map { |num| num + 1 }
+
+p add_one
+
+# add_one = []
+# index = 0
+
+# while index < numbers.length
+#   add_one << numbers[index] + 1
+#   p add_one
+#   index = index + 1
+# end
